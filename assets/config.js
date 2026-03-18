60c47c662f4f11624aa9ec3b237b0e0d912e926e
Smartlinkx <fiber.smartlink@gmail.com>
2026-03-18 19:33:03 +0800

    config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 const APP_CONFIG = {
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxoGl5PemNwfDWQ4EtyUzqsG2EM_WXYgs_ili--J7B246iltJYn0r9qaPn3NvbjHwA_/exec"
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbzrbg78fx1-UFGCbgc7MuMBXI_Bt0M1VrXrOnLmuEhEAymU_K7zoGkSQSlxVmltiix5PQ/exec"
 };
